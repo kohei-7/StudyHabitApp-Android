a21e4fe868c8f08083e6c6ac68592a7009ab0ca8
Ernesto <ejcontrerast@outlook.com>
2024-01-14 05:59:40 -0800

    Ernesto design of timer screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,13 +38,12 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     //Material Design 3 library
-    implementation ("com.google.android.material:material:<version>")
+    implementation ("androidx.compose.material3:material3:1.1.2")
     androidTestImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     //
     //FragmentScenario for testing fragment
@@ -55,5 +54,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
 }
