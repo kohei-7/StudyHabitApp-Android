f1f38e240badd39d55b97103596d429f7fc5f691
Kohei <1lovehamuhamu.15@gmail.com>
2024-01-23 20:43:32 -0800

    addding the function of text watcher

--- app/src/main/java/com/sha/studyhabitapplication/TimerFragment.kt
+++ app/src/main/java/com/sha/studyhabitapplication/TimerFragment.kt
@@ -1,42 +1,58 @@
 package com.sha.studyhabitapplication
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
 
 class TimerFragment : Fragment(R.layout.fragment_timer) {
-    private lateinit var hoursBoxEditTextView :EditText
-    private lateinit var minutesBoxEditTextView: EditText
-    private lateinit var secondsBoxEditTextView: EditText
+    private lateinit var hoursBoxEditTextView : TextInputEditText
+    private lateinit var minutesBoxEditTextView: TextInputEditText
+    private lateinit var secondsBoxEditTextView: TextInputEditText
 
+    //ref: https://www.geeksforgeeks.org/ontextchangedlistener-in-android/
+    private var textWatcher: TextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            Toast.makeText(activity, "before textWatcher", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            Toast.makeText(activity, "changed textWatcher", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            Toast.makeText(activity, "after text changed", Toast.LENGTH_SHORT).show()
+        }
+
+    }
     //Do not access graphical things through onCreate method
     // because this will be called before finishing onCreate() of Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        Toast.makeText(activity, "onCreate method", Toast.LENGTH_LONG).show()
-//        hoursBoxEditTextView.findViewById<>()
-
-
 
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        //doesn't work
-//        hoursBoxEditTextView.findViewById<EditText>(R.id.hours_box)
-//        minutesBoxEditTextView.findViewById<EditText>(R.id.minutes_box)
-//        secondsBoxEditTextView.findViewById<EditText>(R.id.seconds_box)
+        val view: View = inflater.inflate(R.layout.fragment_timer, container, false)
+        hoursBoxEditTextView = view.findViewById(R.id.hours_box)
+        minutesBoxEditTextView = view.findViewById(R.id.minutes_box)
+        secondsBoxEditTextView = view.findViewById(R.id.seconds_box)
 
-//        hoursBoxEditTextView.isClickable = true
-//        minutesBoxEditTextView.isClickable = true
-//        secondsBoxEditTextView.isClickable = true
-        return inflater.inflate(R.layout.fragment_timer, null)
+        hoursBoxEditTextView.addTextChangedListener (textWatcher)
+        return view
     }
     override fun onResume() {
         super.onResume()
-        Toast.makeText(activity, "onResume method", Toast.LENGTH_LONG).show()
+//        Toast.makeText(activity, "onResume method", Toast.LENGTH_LONG).show()
 
     }
 }
